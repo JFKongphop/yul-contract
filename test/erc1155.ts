6e6bfcde82bcaad39ac9319020398514e3c22ce1
JFKongphop <kongphopleo@gmail.com>
2025-07-19 00:59:13 +0700

    add mint

--- test/erc1155.ts
+++ test/erc1155.ts
@@ -11,6 +11,10 @@ describe('ERC1155', async () => {
   let user2: SignerWithAddress;
   let user3: SignerWithAddress;
 
+  const TransferSingle = '0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62';
+  const TransferBatch = '0x4a39dc06d4c0dbc64b70af90fd698a233a518aa5d07e595d983b8c0526c8f7fb';
+  const ApprovalForAll = '0x17307eab39ab6107e8899845ad3d59bd9653f200f220920489ca2b5937696c31';
+
   before(async () => {    
     [user1, user2, user3] = await ethers.getSigners();
     const Contract = await ethers.getContractFactory([], bytecode);
@@ -19,11 +23,22 @@ describe('ERC1155', async () => {
   });
 
   describe('Initial Value', async () => {
-    it('Should return total supply', async () => {
-      // const result = await providerCall('test', [1, user1.address]);
+    it('Should return balanceOf user1', async () => {
+      const result = await providerCall('balanceOf', [user1.address, 1]);
 
-      // console.log(result)
-      // // expect(Number(result)).equal(0);
+      expect(Number(result)).equal(0);
+    });
+  });
+
+  describe('Mint', async () => {
+    it('Should return mint', async () => {
+      await signerCall(user1, 'mint', [user1.address, 1, 1]);
+      const result = await providerCall('balanceOf', [user1.address, 1]);
+
+      const logs = await getLogs(TransferSingle);
+
+
+      console.log(logs)
     });
   });
 });
